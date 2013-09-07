d00909dc88490c4beda4d276ac48c61496d47e1b
sanyamkamat <sanyamkamat@gmail.com>
2013-09-07 19:28:11 +0530

    Adding Chap 02

---
+++ chap2/control_flow.rb
@@ -0,0 +1,23 @@
+a = 2
+b = 2
+
+if a==1
+  puts "a is 1"
+elsif a==2
+  puts "a is 2"
+else
+  puts "I don't know the value of 'a'"
+end
+
+case b
+when 1
+  puts "b is 1"
+when 2
+  puts "b is 2"
+else
+  puts "I don't know the value of 'b'"
+end
+      
+
+
+
